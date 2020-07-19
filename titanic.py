12c612c1efe3b20d29a37dd98a57347abbcc7195
Piyush Kandwal <ultrapiyushkandwal@gmail.com>
2020-07-19 17:38:22 +0530

    Update titanic.py

--- titanic.py
+++ titanic.py
@@ -14,7 +14,6 @@ IMPORTING LIBRARIES
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import ensemble, model_selection
 
 """READING TRANING AND TESTING DATA FROM CSV FILE"""
 
@@ -70,9 +69,14 @@ clean_data(test)
 
 """MODELLING"""
 
+from sklearn.model_selection import cross_val_score
 target = train["Survived"].values
 features_forest = train[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
+test_features_forest = test[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
 
+"""RANDOM FOREST"""
+
+from sklearn.ensemble import RandomForestClassifier
 forest = ensemble.RandomForestClassifier(
     max_depth = 7,
     min_samples_split = 4,
@@ -82,18 +86,93 @@ forest = ensemble.RandomForestClassifier(
 )
 forest = forest.fit(features_forest, target)
 
-print(forest.feature_importances_)
-print(forest.score(features_forest, target))
-
-scores = model_selection.cross_val_score(forest, features_forest, target, scoring='accuracy', cv=10)
-print (scores)
-print (scores.mean())
-
-test_features_forest = test[["Pclass", "Age", "Sex", "Fare", "SibSp", "Parch", "Embarked"]].values
 prediction_forest = forest.predict(test_features_forest)
 
 PassengerId = np.array(test["PassengerId"]).astype(int)
 solution = pd.DataFrame(prediction_forest, PassengerId, columns = ["Survived"])
 solution.to_csv("random_forest.csv", index_label = ["PassengerId"])
 print("Your submission was successfully saved!")
+
+scores = model_selection.cross_val_score(forest, features_forest, target, scoring='accuracy', cv=10)
+print (scores.mean())
+
+"""LOGISTIC REGRESSION"""
+
+from sklearn.linear_model import LogisticRegression
+log_reg = LogisticRegression(max_iter = 1000)
+log_reg = log_reg.fit(features_forest, target)
+
+log_pred=log_reg.predict(test_features_forest)
+
+PassengerId = np.array(test["PassengerId"]).astype(int)
+solution = pd.DataFrame(log_pred, PassengerId, columns = ["Survived"])
+solution.to_csv("logistic regression.csv", index_label = ["PassengerId"])
+print("Your submission was successfully saved!")
+
+scores_log = cross_val_score(log_reg, features_forest, target, scoring='accuracy', cv=10)
+print('Cross-Validation Accuracy Score',scores_log.mean())
+
+"""NAIVE BAIYES"""
+
+from sklearn.naive_bayes import GaussianNB 
+gnb = GaussianNB() 
+gnb.fit(features_forest, target) 
+
+gnb_pred = gnb.predict(test_features_forest) 
+
+PassengerId = np.array(test["PassengerId"]).astype(int)
+solution = pd.DataFrame(gnb_pred, PassengerId, columns = ["Survived"])
+solution.to_csv("naive_bayes.csv", index_label = ["PassengerId"])
+print("Your submission was successfully saved!")
+
+scores_log = cross_val_score(gnb, features_forest, target, scoring='accuracy', cv=10)
+print('Cross-Validation Accuracy Score',scores_log.mean())
+
+"""SVM"""
+
+from sklearn import svm
+sv=svm.SVC(kernel='linear')
+sv.fit(features_forest, target)
+
+sv_pred=sv.predict(test_features_forest)
+
+PassengerId = np.array(test["PassengerId"]).astype(int)
+solution = pd.DataFrame(sv_pred, PassengerId, columns = ["Survived"])
+solution.to_csv("svm.csv", index_label = ["PassengerId"])
+print("Your submission was successfully saved!")
+
+scores_log = cross_val_score(sv, features_forest, target, scoring='accuracy', cv=10)
+print('Cross-Validation Accuracy Score',scores_log.mean())
+
+"""KNN"""
+
+from sklearn.neighbors import KNeighborsClassifier 
+knn = KNeighborsClassifier(n_neighbors=7) 
+knn.fit(features_forest, target)
+
+knn_pred=knn.predict(test_features_forest)
+
+PassengerId = np.array(test["PassengerId"]).astype(int)
+solution = pd.DataFrame(knn_pred, PassengerId, columns = ["Survived"])
+solution.to_csv("knn.csv", index_label = ["PassengerId"])
+print("Your submission was successfully saved!")
+
+scores_log = cross_val_score(knn, features_forest, target, scoring='accuracy', cv=10)
+print('Cross-Validation Accuracy Score',scores_log.mean())
+
+"""DECISION TREE"""
+
+from sklearn.tree import DecisionTreeClassifier
+dst=DecisionTreeClassifier()
+dst.fit(features_forest, target)
+
+dst_pred=dst.predict(test_features_forest)
+
+PassengerId = np.array(test["PassengerId"]).astype(int)
+solution = pd.DataFrame(dst_pred, PassengerId, columns = ["Survived"])
+solution.to_csv("decision_tree.csv", index_label = ["PassengerId"])
+print("Your submission was successfully saved!")
+
+scores_log = cross_val_score(dst, features_forest, target, scoring='accuracy', cv=10)
+print('Cross-Validation Accuracy Score',scores_log.mean())
 
